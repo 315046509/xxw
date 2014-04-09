db3ac589cf26a62262ac21de6e56a9e4bb2c66ad
shichao <315046509@qq.com>
2014-04-09 14:04:25 +0800

    df

--- app/models/journalism.rb
+++ app/models/journalism.rb
@@ -3,6 +3,7 @@ class Journalism < ActiveRecord::Base
   validates :descrpiton, :presence => {:message => "内容不能为空"}
 
   scope :order_id_desc, order("id DESC")
+  scope :order_ct_desc, order("created_at DESC")
 
   QUESTION_CATEGORYS = [["未分类", 0], ["全景校区", 1], ["校内活动", 3], ["历年成绩", 4], ["学校文化", 5], ["教师风采", 6], ["分校查询", 7], ["联系我们", 8]]
 

5f454f4887f17f1ec5823fc92a2fa654cb5c915c
shichao <315046509@qq.com>
2014-04-09 10:42:29 +0800

    t

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -2,7 +2,7 @@ class Roll < ActiveRecord::Base
   validates :url, :presence => {:message => "链接为空！"}
 
   attr_accessor :avatar
-  has_attached_file :avatar, :styles => {:medium => "220x160#", :thumb => "68x48#", :thumb1 => "342x200#", :zuopin => "640x480#"}, :default_url => "/images/:style/missing.png",
+  has_attached_file :avatar, :styles => {:medium => "220x160#", :thumb => "68x48#", :thumb1 => "342x250#", :zuopin => "640x480#"}, :default_url => "/images/:style/missing.png",
                     :url => "/roll/:id_partition/:style/:filename"
   validates_attachment_size :avatar, :less_than => 2.megabytes
   validates_attachment_content_type :avatar, :content_type => ['image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/jpg']

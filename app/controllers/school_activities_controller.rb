68b2fb05b62233f494bff0acaa22eb607f674918
shichao <315046509@qq.com>
2014-04-10 08:34:31 +0800

    提交

--- app/controllers/school_activities_controller.rb
+++ app/controllers/school_activities_controller.rb
@@ -2,7 +2,7 @@ class SchoolActivitiesController < ApplicationController
   def index
     # 校园活动
     @category_2_name = Journalism.question_category_name(3)
-    @category_2_questions = Journalism.where({:question_category_id => 3}, "title <> ''").order_ct_desc.page(params[:page]).per(10)
+    @category_2_questions = Journalism.where({:question_category_id => 3}, "title <> ''").page(params[:page]).per(10)
   end
 
   def show

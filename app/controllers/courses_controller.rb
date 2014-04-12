eb02a148fa4416a63dfb452901c862242da2b25a
shichao <315046509@qq.com>
2014-04-12 11:30:06 +0800

    提交

--- app/controllers/courses_controller.rb
+++
@@ -1,9 +0,0 @@
-class CoursesController < ApplicationController
-  def index
-    @category_2_questions = Course.where("title <> ''").order_ct_desc.page(params[:page]).per(10)
-  end
-
-  def show
-    @journalism = Course.find params[:id]
-  end
-end

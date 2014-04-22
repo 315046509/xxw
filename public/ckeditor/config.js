bde5d1e3d4349b2abe3816514437c25d877949b8
shichao <315046509@qq.com>
2014-04-22 13:18:55 +0800

    tijiao

---
+++ public/ckeditor/config.js
@@ -0,0 +1,17 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    config.width = 900;
+    config.height = 400;
+    //工具栏是否可以被收缩
+    config.toolbarCanCollapse = true;
+    //工具栏默认是否展开
+    config.toolbarStartupExpanded = false;
+
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+};
